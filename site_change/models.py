1b447c779913c39715aa23265d62c0adcda27bfe
Kamila <kamieva_k@iuca.kg>
2024-10-21 14:01:30 +0600

    created models

--- site_change/models.py
+++ site_change/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
+    address = models.CharField(max_length=255)
+    trade_for = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
+    
