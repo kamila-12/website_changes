18769bd9938f08d30bb9993f00f447b560fe8e30
lina0752 <urmanbetova_a@iuca.kg>
2024-10-23 16:49:58 +0600

    views, serializer

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+# API для списка продуктов
+class ProductListAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Аутентификация только для создания
+
+# API для детального просмотра и редактирования продукта
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Аутентификация для редактирования и удаления
